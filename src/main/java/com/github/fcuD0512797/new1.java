090180b923f3beec778395ac502608eb4c6ebdd4
PC234-03\PC234 <jsbach4770@gmail.com>
2017-12-06 12:02:47 +0800

    new1

---
+++ src/main/java/com/github/fcuD0512797/new1.java
@@ -0,0 +1,12 @@
+package com.github.fcuD0512797;
+
+public class new1 {
+    public int test(int num)
+    {
+        if(num<10)
+        {
+            System.out.println("Hello");
+        }
+        return num;
+    }
+}
---
+++ src/test/java/com/github/fcuD0512797/new1Test.java
@@ -0,0 +1,13 @@
+package com.github.fcuD0512797;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class new1Test {
+    @Test
+    public void test1() throws Exception {
+        assertEquals(0,0);
+    }
+
+}
